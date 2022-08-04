07eb13e73443237f2256ae5e79e2ea3c471d24b7
Mgoumane <goumane.mahmoudou@gmail.com>
2022-08-04 15:28:49 +0200

    cours terminée

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home",{
+    title: 'Ma homepage'
+  });
 });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.use(express.urlencoded({ extended: true }))
+app.post("/login/register", (req, res) => {
+  res.send("Donnée envoyée");
+});
+
+
 
 app.listen(8000, () => {
   console.log("Serveur lancé");
